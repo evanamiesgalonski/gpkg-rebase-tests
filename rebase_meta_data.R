a1a4867a077b87e940cb51965261bf16adfee8e7
Evan <evanamiesgalonski@gmail.com>
2023-04-05 14:36:57 -0700

    dput

---
+++ rebase_meta_data.R
@@ -0,0 +1,7 @@
+structure(list(id = 1, auxiliary_storage_labeling_positionx = 505861.730254211, 
+    auxiliary_storage_labeling_positiony = 5497137.60492412, 
+    auxiliary_storage_labeling_labelrotation = 325.476271876828), row.names = c(NA, 
+-1L), class = c("tbl_df", "tbl", "data.frame"), sf_column = "geom", agr = structure(c(id = NA_integer_, 
+auxiliary_storage_labeling_positionx = NA_integer_, auxiliary_storage_labeling_positiony = NA_integer_, 
+auxiliary_storage_labeling_labelrotation = NA_integer_), class = "factor", levels = c("constant", 
+"aggregate", "identity")))
