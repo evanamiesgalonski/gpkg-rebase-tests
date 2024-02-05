7cad764516bf7af21ab30bed1d3add0f0a2e8883
Evan <evanamiesgalonski@gmail.com>
2024-02-05 09:53:48 -0800

    update rebase script template

--- rebase-gpkg.R
+++ rebase-gpkg.R
@@ -1,91 +1,30 @@
-#### rebase gpkg ####
-# the idea is to add label positioning meta data to gpkgs as proper data columns
-# these can then be read in again to R and joined to the original data.
-# this allows the user to re-export data without losing work done in QGIS.
-# stores copies of modified gpkgs in another folder (gpkg_rebase) so that the 
-# user doesnt accidentally overwrite data in the main workup folder and lose changes.
-
-# need a package for some basic functions, workflow will be designed so a user 
-# can run the same code whether its an initial workup or a rebase
-
-# note 1: could also be part of a larger package that creats QLM files for storage
-# of more complex meta data that connot be stored as columns:
-# (layer symbology, rendering, grouping, etc)
-
-# note 2: need a procces to dput the modifications after read in
-# that then gets sourced to reproduce the objects. should actually do on 
-# initial workup, then code blocks get updated on each rebase. functions should 
-# operate on a separate file that contains hard coded symbology or label info, 
-# if no rebasable files are available then it should create template verisons 
-# in that file this way there would be a hard coded version of the meta data
-# that's backed up (github) and we aren't relying on some data sitting in a folder on dropbox.
-
-
-
-#### basic example of process: ####
-
 source("header.R")
+source("gpkg-rebase-functions.R")
+# 1 - read worked up  spatial data
 
-# function 1 - save_rebase - make copy of all files in gpkg folder (empty on first workup)
+sbf_set_sub("gpkg_spatial")
+sbf_load_datas()
+
+keys <- sbf_load_object("keys")
+crs <- 26911
+
 sbf_set_main(spatial_dir)
+sbf_reset_sub()
 
-dir <- sbf_get_main()
-gpkg_dir <- file.path(dir, "gpkg")
-rebase_dir <- file.path(dir, "gpkg_rebase")
+# 2 - save any gpkgs that have not yet been exported
+gpkg_dir <- file.path(spatial_dir, "gpkg")
+save_new_gpkgs(gpkg_dir, keys)
 
-files <- list.files(gpkg_dir, full.names = TRUE, recursive = TRUE)
-if(!dir.exists(rebase_dir)) dir.create(rebase_dir)
-file.copy(files, rebase_dir, overwrite = TRUE)
+# 3 - read meta data, supply empty meta data cols for new data.
+gpkg_files <- list.files(gpkg_dir, pattern = ".gpkg$", full.names = TRUE)
+gpkg_names <- basename(gpkg_files) %>% str_replace(".gpkg$", "")
+meta_list <- read_meta(gpkg_files, keys)
 
-# note: spatial exports will now require a list of primary keys for all datas 
-# so that meta data added in QGIS can be joined to the original data.
-keys <- c("loc" = "id")
+# 4 - serialize - write meta data to R file (overwrites contents)
+serialize_meta(meta_list)
 
-# original raw data workup occurs here
-loc <- tibble(
-  id = 1, X = 505571, Y = 5496619
-) %>%
-  ps_coords_to_sfc(crs = 26911)
+# 5 - rebase - read meta data construct and overwrite local meta data
+# this way we rely on hard coded meta data in case everything else gets lost.
+rebase_meta()
 
-# include function 2 as part of read_rebase?
-# function 2 - rebase - reads and cleans modified files (returns nothing if rebase dir empty)
-# would be good to have function sbf_load_gpkgs, and maybe even sbf_load_gpkg_rebase for this
-rebase_files <- list.files(rebase_dir, full.names = TRUE, recursive = TRUE, pattern = ".gpkg$")
-
-if(length(rebase_files)) {
- 
-  x_name <- "loc"
-  meta_cols <- c( # should add call out line and feature rotation cols
-    "auxiliary_storage_labeling_positionx",
-    "auxiliary_storage_labeling_positiony",
-    "auxiliary_storage_labeling_labelrotation"
-  )
-  key <- keys[x_name] %>% unname()
-  cols <- c(key, meta_cols)
-  
-  loc_rebased <- st_read(rebase_files[1], quiet = TRUE) %>%
-    as_tibble() %>%
-    select(all_of(cols))
-  
-  # note: dput reproduction would occur here
-  
-  # function 3 - apply_rebase- matches rebased to og files based on name,
-  # then joins rebased data based on provided key
-  
-  loc %<>% left_join(loc_rebased %>% as_tibble, by = key)  
-  
-}
-
-# add meta columns only if not added by rebase (first workup)
-loc %<>% add_label_meta()
-
-# must clean up rebase files so they dont get saved in original workup folder
-rm(list = ls()[str_detect(ls(), "rebased")])
 sbf_save_gpkgs()
-
-# QGIS Notes:
-# must use each datas 'key' for label storage when starting to move labels
-
-# works best to highjack QGIS default column names for label positioning:
-# eg. "auxiliary_storage_labeling_positionx" as this way you dont have to manually
-# set the position storage columns
